890ce3d5bb9caae23376ef93280823e4290ac8e4
riv2r <3237208018@qq.com>
2022-07-05 13:17:26 +0800

    modify InitPsychtoolbox and README

--- program/InitPsychtoolbox.m
+++ program/InitPsychtoolbox.m
@@ -2,6 +2,6 @@ function output = InitPsychtoolbox()
 
 cd C:\Users\user\AppData\Roaming\MathWorks\'MATLAB Add-Ons'\Collections\Psychtoolbox-3\Psychtoolbox\
 SetupPsychtoolbox(1)
-cd C:\Users\user\Desktop\ControlByBCInVision\program\
+cd C:\Users\user\Desktop\ControlByBCI\program\
 
 end

58ab68f4541891a2394623a9d8240f84ce0f9540
riv2r <3237208018@qq.com>
2022-07-22 16:17:04 +0800

    recent final version (DeepCCA.py)

--- program/DeepCCA.py
+++ program/DeepCCA.py
@@ -1,48 +1,54 @@
 import torch
 import torch.nn as nn
 
+
 class MlpNet(nn.Module):
 
     def __init__(self,layer_size,device=torch.device('cpu')):
+        
         super(MlpNet,self).__init__()
         self.device = device
         layers=[]
-        for lIdx in range(len(layer_size)-1):
-            if lIdx == len(layer_size)-2:
+        for layer_Idx in range(len(layer_size)-1):
+            if layer_Idx == len(layer_size)-2:
                 layers.append(
                     nn.Sequential(
-                        nn.BatchNorm1d(num_features=layer_size[lIdx],affine=False),
-                        nn.Linear(layer_size[lIdx],layer_size[lIdx+1])
+                        nn.BatchNorm1d(num_features=layer_size[layer_Idx],affine=False),
+                        nn.Linear(layer_size[layer_Idx],layer_size[layer_Idx+1])
                     )
                 )
             else:
                 layers.append(
                     nn.Sequential(
-                        nn.Linear(layer_size[lIdx],layer_size[lIdx+1]),
+                        nn.Linear(layer_size[layer_Idx],layer_size[layer_Idx+1]),
                         nn.Sigmoid(),
-                        nn.BatchNorm1d(num_features = layer_size[lIdx+1],affine=False)
+                        nn.BatchNorm1d(num_features = layer_size[layer_Idx+1],affine=False)
                     )
                 )
         self.layers = nn.ModuleList(layers)
     
     def forward(self,x):
+
         # forward propagation function
         for layer in self.layers:
             # avoid this compute part be moved on CPU
             x = x.to(self.device)
             layer = layer.to(self.device)
             x = layer(x)
+
         return x
 
 
 class CCALoss():
     
     def __init__(self,output_size,use_all_singular_values,device):
+
         self.output_size = output_size
         self.use_all_singular_values = use_all_singular_values
         self.device = device
     
     def loss(self,H1,H2):
+        
         r1 = 1e-3
         r2 = 1e-3
         eps = 1e-9
@@ -52,20 +58,16 @@ class CCALoss():
         H1, H2 =H1.t(), H2.t()
         # H1 (6,1250)
         # H2 (8,1250)
-
         o1 = H1.size(0)
         o2 = H2.size(0)
         m = H1.size(1)
-
         # centered data
         # H.mean(dim=1) compute row's mean
         H1bar = H1 - H1.mean(dim=1).unsqueeze(dim=1)
         H2bar = H2 - H2.mean(dim=1).unsqueeze(dim=1)
-
         sigmaH12 = (1.0/(m-1))*torch.matmul(H1bar,H2bar.t())
         sigmaH11 = (1.0/(m-1))*torch.matmul(H1bar,H1bar.t())+r1*torch.eye(o1,device=self.device)
         sigmaH22 = (1.0/(m-1))*torch.matmul(H2bar,H2bar.t())+r2*torch.eye(o2,device=self.device)
-
         # Eigenvalue Decomposition
         [D1,V1]=torch.symeig(sigmaH11,eigenvectors=True)
         [D2,V2]=torch.symeig(sigmaH22,eigenvectors=True)
@@ -77,7 +79,6 @@ class CCALoss():
         posIdx2 = torch.gt(D2,eps).nonzero()[:,0]
         D2 = D2[posIdx2]
         V2 = V2[:,posIdx2]
-
         sigmaH11Inv = torch.matmul(
             torch.matmul(V1, torch.diag(D1 ** -0.5)),
             V1.t()
@@ -86,12 +87,10 @@ class CCALoss():
             torch.matmul(V2, torch.diag(D2 ** -0.5)),
             V2.t()
         )
-
         Tval = torch.matmul(
             torch.matmul(sigmaH11Inv,sigmaH12),
             sigmaH22Inv
         )
-
         if self.use_all_singular_values:
             temp = torch.matmul(Tval.t(),Tval)
             corr = torch.sqrt(torch.trace(temp))
@@ -109,13 +108,15 @@ class CCALoss():
 class DeepCCA(nn.Module):
 
     def __init__(self,layer1_size,layer2_size,output_size,use_all_singular_values,device=torch.device('cpu')):
-        super().__init__()
+
+        super(DeepCCA,self).__init__()
         self.model1 = MlpNet(layer1_size,device).double()
         self.model2 = MlpNet(layer2_size,device).double()
         
         self.loss = CCALoss(output_size,use_all_singular_values,device).loss
         
     def forward(self,x1,x2):
+
         output1 = self.model1(x1)
         output2 = self.model2(x2)
 
